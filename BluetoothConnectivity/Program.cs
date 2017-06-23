b9bbf5d3853f82e33faefd224f1bd79f741231a0
Stadzior <Stadzior@gmail.com>
2017-06-23 23:37:09 +0200

    worx

--- BluetoothConnectivity/Program.cs
+++ BluetoothConnectivity/Program.cs
@@ -13,21 +13,30 @@ namespace BluetoothConnectivity
     {
         static void Main(string[] args)
         {
+            var radioB = BluetoothRadio.AllRadios;
             var _service = new BluetoothSenderService();
+            var _service2 = new BluetoothSenderService();
 
             Console.WriteLine("Looking for the devices...");
             var discoveredDevices = _service.GetDevices().Result;
+            var arduinoName = "HC-06";
+            var phoneName = "ALCATEL ONETOUCH POP C7";
+            Console.WriteLine($"Retrieving {arduinoName}...");
+            var arduinoDevice = discoveredDevices.Single(d => d.Name.Equals(arduinoName));
+            var phoneDevice = discoveredDevices.Single(d => d.Name.Equals(phoneName));
 
-            Console.WriteLine("Retrieving CRIUS_BT...");
-            var carDevice = discoveredDevices.Single(d => d.Name.Equals("CRIUS_BT"));
+            if (!arduinoDevice.Remembered)
+                _service.Pair(arduinoDevice);
 
-            if (!carDevice.Remembered)
-                _service.Pair(carDevice);
+            if (!phoneDevice.Remembered)
+                _service.Pair(phoneDevice);
 
-            Console.WriteLine("Connecting...");
-            _service.Connect(carDevice, BluetoothService.SerialPort);
+            Console.WriteLine($"Connecting to {arduinoName}..."); 
+            var connectedToArduino = _service.Connect(arduinoDevice, BluetoothService.SerialPort);
+            Console.WriteLine($"Connecting to {phoneName}...");
+            var connectedToPhone = _service2.Connect(phoneDevice, BluetoothService.SerialPort);
 
-            if (carDevice.IsConnected)
+            if (connectedToArduino && connectedToPhone)
             {
                 Console.WriteLine("Connected...");
                 var signal = '0';
@@ -37,7 +46,7 @@ namespace BluetoothConnectivity
                     if(input.Length > 0)
                     {
                         signal = input[0];
-                        var sended = _service.Send(carDevice, signal).Result;
+                        var sended = _service.Send(arduinoDevice, signal).Result && _service2.Send(phoneDevice, signal).Result;
                         if (sended)
                             Console.WriteLine($"Sended signal: {signal}");
                         else
